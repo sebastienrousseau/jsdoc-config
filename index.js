ef9b395a3f9736eace6ea772acb29f36b664077d
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-05-22 09:15:11 +0100

    v0.0.3

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 module.exports = {
-  "source": {
-    "includePattern": ".+\\.js(doc|x)?$",
-    "include": "./src/**/*.js"
-    "exclude": ["node_modules", "dist", "coverage", "test"]
-  },
-  "recurseDepth": 10,
   "opts": {
-    "destination": "./docs/",
-    "recurse": true
+    "destination": "./documentation/",
+    "encoding": "utf8",
+    "recurse": true,
+    "template": "./node_modules/minami",
+    "verbose": true
+  },
+  "plugins": [
+    "plugins/markdown"
+  ],
+  "recurseDepth": 10,
+  "source": {
+    "exclude": ["node_modules", "dist", "coverage", "test"],
+    "excludePattern": "(node_modules/|documentation)",
+    "include": ["src", "README.md"],
+    "includePattern": ".js$"
+  },
+  "tags": {
+    "allowUnknownTags": true,
+    "dictionaries": ["jsdoc"]
+  },
+  "templates": {
+    "cleverLinks": false,
+    "monospaceLinks": true,
+    "showInheritedInNav": true,
+    "useLongnameInNav": false
   }
 };
