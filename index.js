7c9aede0b3c8082489ea8f36205522f049378245
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-05-22 10:25:38 +0100

    v0.0.4

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-module.exports = {
-  "opts": {
-    "destination": "./documentation/",
-    "encoding": "utf8",
-    "recurse": true,
-    "template": "./node_modules/minami",
-    "verbose": true
-  },
-  "plugins": [
-    "plugins/markdown"
-  ],
-  "recurseDepth": 10,
-  "source": {
-    "exclude": ["node_modules", "dist", "coverage", "test"],
-    "excludePattern": "(node_modules/|documentation)",
-    "include": ["src", "README.md"],
-    "includePattern": ".js$"
-  },
-  "tags": {
-    "allowUnknownTags": true,
-    "dictionaries": ["jsdoc"]
-  },
-  "templates": {
-    "cleverLinks": false,
-    "monospaceLinks": true,
-    "showInheritedInNav": true,
-    "useLongnameInNav": false
+module.exports = jsDocConfig => {
+  return {
+    "opts": {
+      "destination": "./documentation/",
+      "encoding": "utf8",
+      "recurse": true,
+      "template": "./node_modules/minami",
+      "verbose": true
+    },
+    "plugins": [
+      "plugins/markdown"
+    ],
+    "recurseDepth": 10,
+    "source": {
+      "exclude": ["node_modules", "dist", "coverage", "test"],
+      "excludePattern": "(node_modules/|documentation)",
+      "include": ["src", "README.md"],
+      "includePattern": ".js$"
+    },
+    "tags": {
+      "allowUnknownTags": true,
+      "dictionaries": ["jsdoc"]
+    },
+    "templates": {
+      "cleverLinks": false,
+      "monospaceLinks": true,
+      "showInheritedInNav": true,
+      "useLongnameInNav": false
+    }
   }
 };
